e49453e8a6684412f949a33a8d7a8d00c088c54d
Pauli Salmenrinne <susundberg@gmail.com>
2017-05-29 10:04:20 +0300

    Required fakes.

---
+++ tests/mocks_man/RTClib.h
@@ -0,0 +1,13 @@
+
+#ifndef FAKE_RTCLIB_H
+#define FAKE_RTCLIB_H
+
+class RTC_DS1307
+{
+   
+
+};
+
+
+#endif
+
---
+++ tests/mocks_man/SparkFunHTU21D.h
@@ -0,0 +1,11 @@
+#ifndef FAKE_SPARKFUN_HTU21D_H
+#define FAKE_SPARKFUN_HTU21D_H
+
+class HTU21D
+{
+   float readTemperature();
+   float readHumidity();
+};
+
+
+#endif
